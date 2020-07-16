3ea04d4ae8a80201e71592d7b5fd81b30c8d36b2
guillaume.sadler <guillaume.sadler@gmail.com>
2020-07-16 11:08:40 +0200

    add vagrant to docker group

--- vmprovision.sh
+++ vmprovision.sh
@@ -33,6 +33,9 @@ sudo apt-get -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get -y  install ansible
 
+# ADD VAGRANT TO DOCKER GROUP
+usermod -aG docker vagrant
+
 # DOCKER PROJECT INSTALL
 git clone https://github.com/GuillaumeSadlerOC/DevOps_Docker.git /home/vagrant/datas/DevOps_Docker
 sudo docker build -t supervisor_nginx_sshd /home/vagrant/datas/DevOps_Docker/
